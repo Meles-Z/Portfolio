f645646eb151eb125a73613cd2c8ebd8bccb8e9e
Meles-Z <meles.zawdie@gmail.com>
2024-11-01 15:55:08 +0300

    My portifolio

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,35 @@
 <script>
-    import "../app.css";
-  </script>
-  
+  import "../app.css";
+  import Footer from "../components/Footer.svelte";
+  import Header from "../components/Header.svelte";
+
+  let y;
+  let innerWidth = 0;
+  let innerHeight = 0;
+
+  function goToTop() {
+    document.body.scrollIntoView();
+  }
+</script>
+
+<div
+  class="relative flex flex-col max-w-[1400px max-auto w-full text-sm sm:text-base min-h-screen"
+>
+  <div
+    class="{'fixed bottom-0 w-full duration-200 flex p-10 z-[10]' +
+      (y > 0
+        ? 'opacity-full pointer-events-auto'
+        : 'pointers-events-none opacity-0')}"
+  >
+    <button
+      on:click="{goToTop}"
+      class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer"
+      ><i class="fa-solid fa-arrow-up"></i></button
+    >
+  </div>
+  <Header {y} />
   <slot />
+  <Footer />
+</div>
+
+<svelte:window bind:scrollY="{y}" bind:innerHeight bind:innerWidth />
